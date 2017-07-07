8b9719a62614d088398fd9f50b1e626b7267ab22
Joshua Steubs <joshua.steubs@gmail.com>
2017-07-07 13:35:39 -0700

    added loop and fixed terminate

--- quick.py
+++ quick.py
@@ -1,27 +1,30 @@
-import wikipedia
+import wikipedia as wiki
 import argparse
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-t" "--type", 
-	             required=True, 
-	             help="type of search to use (wiki or word search)")
-ap.add_argument("-w" "--word", 
-	             required=True, 
-	             help="the word you'd like to be searched")
-
-args = vars(ap.parse_args())
-
-search_engine = args['type']
-word = args["word"]
 
 def wiki_search(word):
-	print(wikipedia.summary(word))
+	search_result_list = wiki.search(word, results=5)
+	count = 1
+	print('\nWhich result did you mean? Choose corresponding number:')
+	for entry in search_result_list:
+		print(str(count)+')'+entry)
+		count = count+1
+	user_choice = input('\n>>')
+	print(
+		search_result_list[int(user_choice)-1]+
+		":\n\n"+
+		wiki.summary(search_result_list[int(user_choice)-1])+
+		"\n\n")
 
-wiki_search(word)
+def main():
+	print('\n\nWelcome to Quick Wiki Search. A command line tool for easy wikipedia search. Enter a word to get the summary. Enter \'q\' to exit.')
+	user_input = input('>>')
+	while(user_input is not 'q'):	
+		wiki_search(user_input)
+		user_input = input('\n>>')
 
-
-
-
+if __name__ == '__main__':
+	main()
 
 
 
