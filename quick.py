abb73a2a14b56b4f913423798ffa48a0da540c57
Joshua Steubs <joshua.steubs@gmail.com>
2017-10-11 17:53:30 -0700

    fixed occassional disambiguation error

--- quick.py
+++ quick.py
@@ -2,51 +2,24 @@ import wikipedia as wiki
 import argparse
 import summary_tool
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--term',
+	                dest='term', 
+	                type=str)
+
+search_term = parser.parse_args()
+search_term = search_term.term
+
+
 def wiki_search(word):
-	search_result_list = wiki.search(word, results=5)
-	count = 1
-	print('\nWHICH ONE DID YOU MEAN? CHOOSE CORRESPONDING NUMBER:')
-	for entry in search_result_list:
-		print('   '+str(count)+')  '+entry)
-		count = count+1
-	user_choice = input('\n>>')
-	try:
-		search_result = search_result_list[int(user_choice)-1]
-		title = search_result
-		page = wiki.page(search_result)
-		page_content = page.content
-		st = summary_tool.SummaryTool()
-		sentences_dic = st.get_sentences_ranks(page_content)
-		summary = st.get_summary(title, page_content, sentences_dic)
-		print (summary)
-		#str_to_print = (search_result_list[int(user_choice)-1]+
-					#	":\n\n"+
-					#	wiki.summary(search_result_list[int(user_choice)-1])+
-					#	"\n\n")
-		#set the above string to a variable before printing to allow for exception to be thrown before printing
-		#print(str_to_print)
-	except wiki.exceptions.DisambiguationError as e:
-		#this area still requires a fix. Some warnings about BeautifulSoup's html parser come up, as well as suggest() seems to always return None type
-		#catches case where wiki returns a list rather than a single wiki page
-		#will use wiki's best suggestion instead
-		
-
-
-		#wiki_page = wiki.suggest(search_result_list[int(user_choice)-1])
-		#print(wiki.suggest(search_result_list[int(user_choice)-1]))
-		#print(wiki_page)
-		print('Something isn\'t working.')
-		
-
-
+	search_result = wiki.page(word)
+	st = summary_tool.SummaryTool()
+	summary = st.get_summary(search_result.title, 
+									   search_result.content, 
+									   st.get_sentences_ranks(search_result.content))
+	print(summary)
 def main():
-	print('\n\n/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/==/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/')
-	print('\n   WELCOME TO WIKI QUICK SEARCH. A COMMAND LINE TOOL FOR EASY WIKI SEARCH. ENTER A WORD TO GET THE SUMMARY. ENTER \'q\' TO EXIT.\n')
-	print('/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/==/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/\n')
-	user_input = input('>>')
-	while(user_input is not 'q'):	
-		wiki_search(user_input)
-		user_input = input('\n>>')
+	wiki_search(search_term)
 
 
 if __name__ == '__main__':
